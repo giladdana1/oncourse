02d27e44673170506f06b678cc883a07c541a052
Gilad Dana <gilad2904@gmail.com>
2012-09-14 13:50:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oncourse::Application.config.secret_token = '4aff1199007e4c1cb1683175e434b40889fbfaec6d00b9a0c1d3fcdfc6892866ed87dc17339bc8e0a776dcb4b555da31d7b266c8f05016721e06478b5a4b0571'
