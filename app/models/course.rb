5f4f2d8bd295f00fc2c9988dec4d5e78e320f3ac
Gilad Dana <gilad2904@gmail.com>
2012-09-15 21:39:50 +0000

    Validations commit

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ActiveRecord::Base
-end
+  validates :title, :presence => true, :uniqueness => true, :length => { :maximum => 10 }
+  validates :description, :presence => true, :length => { :maximum => 100 }
+  validates :price, :presence => true, :numericality => { :greater_than_or_equal_to => 5.0}
+  validates :seats, :presence => true
+  end
